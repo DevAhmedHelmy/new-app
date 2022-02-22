83f501ec332905f06b03283696612c43c105f48a
Rana Hisham Foad <rana@megatrust.co>
2022-02-22 10:45:51 +0200

    ahmed

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from 'app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameElementRef: ElementRef;
+  @ViewChild('amountInput') amountElementRef: ElementRef;
+  @Output() ingredientAdd = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onAddItem() {
+    const newIngredient = new Ingredient(
+      this.nameElementRef.nativeElement.value,
+      this.amountElementRef.nativeElement.value
+    );
+    this.ingredientAdd.emit(newIngredient);
   }
-
-  onAddList(){}
-
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,18 +4,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
-  ingredients : Ingredient[]=[
-    new Ingredient("ingredient",5),
-    new Ingredient("ingredient2",6),
-    new Ingredient("ingredient3",7),
-    new Ingredient("ingredient4",8),
+  constructor() {}
+  ingredients: Ingredient[] = [
+    new Ingredient('ingredient', 5),
+    new Ingredient('ingredient2', 6),
+    new Ingredient('ingredient3', 7),
+    new Ingredient('ingredient4', 8),
   ];
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  addNewIngreient(ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
