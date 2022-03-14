81475336c7e066869a96c1c608ddea3851d8a987
Rana Hisham Foad <rana@megatrust.co>
2022-03-14 15:02:43 +0200

    ahmed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
-import {AuthInterceptorService} from "./auth/services/auth-interceptor.service";
+import { AuthInterceptorService } from './auth/services/auth-interceptor.service';
+import { RecipesModule } from './recipes/recipes.module';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
+
     AuthComponent,
     LoadingSpinnerComponent,
   ],
@@ -43,8 +34,17 @@ import {AuthInterceptorService} from "./auth/services/auth-interceptor.service";
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RecipesModule,
   ],
-  providers: [ShoppingListService, RecipeService,{provide:HTTP_INTERCEPTORS,useClass: AuthInterceptorService,multi:true}],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
