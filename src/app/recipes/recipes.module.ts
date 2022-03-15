ab69e26bf520ca514a99e812457d1c495e672e74
ahmed helmy <helmyahmed248@gmail.com>
2022-03-15 00:06:30 -0400

    ahmed

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,5 +1,4 @@
 import { ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -7,9 +6,8 @@ import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.compo
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes.component';
-import { DropdownDirective } from 'app/shared/dropdown.directive';
-
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from 'app/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -19,8 +17,7 @@ import { RecipesRoutingModule } from './recipes-routing.module';
     RecipeItemComponent,
     RecipeStartComponent,
     RecipeEditComponent,
-    DropdownDirective,
   ],
-  imports: [RecipesRoutingModule, ReactiveFormsModule, CommonModule],
+  imports: [RecipesRoutingModule, ReactiveFormsModule, SharedModule],
 })
 export class RecipesModule {}
